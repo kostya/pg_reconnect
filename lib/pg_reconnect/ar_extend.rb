0201138c13a44a7dcb1bd01f86dbb870127c5617
Konstantin Makarchev <kostya27@gmail.com>
2012-12-18 15:49:37 +0400

    # fix 3.0 version

--- lib/pg_reconnect/ar_extend.rb
+++ lib/pg_reconnect/ar_extend.rb
@@ -2,8 +2,8 @@ require 'active_record'
 require 'active_record/connection_adapters/postgresql_adapter'
 
 # Hackety monkey patch
-if ActiveRecord::VERSION::STRING < '3'
+if ActiveRecord::VERSION::STRING < '3.1'
   require File.join(File.dirname(__FILE__), %w{ar23})
-elsif ActiveRecord::VERSION::STRING >= '3'
+elsif ActiveRecord::VERSION::STRING >= '3.1'
   require File.join(File.dirname(__FILE__), %w{ar3})
 end
