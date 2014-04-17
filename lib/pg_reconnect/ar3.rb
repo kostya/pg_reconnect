72357bc08ce11260ad5483a92b557d26774ccc89
Konstantin Makarchev <kostya27@gmail.com>
2014-04-17 17:39:27 +0400

    fix logs for ar3

--- lib/pg_reconnect/ar3.rb
+++ lib/pg_reconnect/ar3.rb
@@ -11,7 +11,7 @@ class ActiveRecord::ConnectionAdapters::PostgreSQLAdapter
   rescue ActiveRecord::StatementInvalid => ex
     unless active?        
       @retry_counter ||= 0
-      @logger.warn "Reconnecting to database after PGError! Try ##{@retry_counter + 1}/#{MAX_QUERY_RETRIES} #{ex.message}, trace: #{ex.backtrace.inspect}"
+      send(:log, Logger::WARN) { "Reconnecting to database after PGError! Try ##{@retry_counter + 1}/#{MAX_QUERY_RETRIES} #{ex.message}, trace: #{ex.backtrace.inspect}" }
 
       if @retry_counter < MAX_QUERY_RETRIES
         @retry_counter += 1
